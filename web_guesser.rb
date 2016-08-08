14a0b39b1467afe08ae596dc51f16a4a8d41133e
PlanetEfficacy <jspevack@gmail.com>
2016-08-08 14:13:09 -0600

    I3: number guessing game complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,7 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-
 class RandomNumberGenerator
 
   attr_reader :number
@@ -14,10 +13,28 @@ class RandomNumberGenerator
     "The SECRET NUMBER is #{number}"
   end
 
+  def success
+    "You got it right!\nThe SECRET NUMBER is #{number}"
+  end
+
+  def check_guess(guess)
+    return "" unless guess
+    guess = guess.to_i
+    return "Way too high!" if guess > number + 5
+    return "Way too low!" if guess < number - 5
+    return "Too high!" if guess > number
+    return "Too low!" if guess < number
+    return success if guess == number
+  end
+
 end
 
 rng = RandomNumberGenerator.new(100)
 
+set :number, rng.number
+
 get '/' do
-  erb :index, :locals => {:number => rng.number }
+  guess = params["guess"]
+  message = rng.check_guess(guess)
+  erb :index, :locals => {:number => settings.number, :message => message}
 end
--- web_guesser_test.rb
+++ web_guesser_test.rb
@@ -6,7 +6,7 @@ class WebGuesserTest < Minitest::Test
 
   def test_it_can_return_random_number
     actual = RandomNumberGenerator.new(100)
-    random_number = actual.random_number
+    random_number = actual.number
     assert_equal true,  random_number >= 0 && random_number <= 100
   end
 
@@ -15,5 +15,15 @@ class WebGuesserTest < Minitest::Test
     length = actual.message.length
     assert_equal true, length == 24 || length == 22 || length == 23
   end
-  
+
+  def test_it_can_return_feedback_that_a_guess_is_too_high
+    actual = RandomNumberGenerator.new(100)
+    assert_equal "Way too high!", actual.feedback(1000)
+  end
+
+  def test_it_can_return_feedback_that_a_gues_is_too_low
+    actual = RandomNumberGenerator.new(100)
+    assert_equal "Way too low!", actual.feedback(-1)
+  end
+
 end
